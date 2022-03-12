5212e0ed9efcbc4c415a33818014a0ae82efa34a
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-13 00:31:26 +0800

    Update AppStoreCustomers.sql

--- sql/AppStoreCustomers.sql
+++ sql/AppStoreCustomers.sql
@@ -4,4 +4,4 @@
 
 ****************************/
 
-INSERT INTO customers VALUES ('John1997', 'John', 'Ng', 'johnng1997@gmail.com', '1997-05-20', '98765432');
+INSERT INTO customers VALUES ('John1997', 'John', 'Ng', 'johnng1997@gmail.com', '1997-05-20', 98765432);
