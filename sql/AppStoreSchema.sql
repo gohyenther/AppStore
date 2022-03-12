8c0e732391abb68ccf98c9f9fd203abf3caf5362
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-13 00:13:23 +0800

    Update AppStoreSchema.sql

--- sql/AppStoreSchema.sql
+++ sql/AppStoreSchema.sql
@@ -5,14 +5,16 @@
 ********************/
 
 CREATE TABLE IF NOT EXISTS customers (
- first_name VARCHAR(64) NOT NULL,
- last_name VARCHAR(64) NOT NULL,
- email VARCHAR(64) UNIQUE NOT NULL,
- dob DATE NOT NULL,
- since DATE NOT NULL,
- customerid VARCHAR(16) PRIMARY KEY,
- country VARCHAR(16) NOT NULL);
+	customerid VARCHAR(16) PRIMARY KEY,
+ 	first_name VARCHAR(64) NOT NULL,
+ 	last_name VARCHAR(64) NOT NULL,
+ 	email VARCHAR(64) UNIQUE NOT NULL,
+ 	dob DATE NOT NULL,
+	contact no. NUMERIC(8) NOT NULL);
 	
+
+/*
+
 CREATE TABLE IF NOT EXISTS games(
  name VARCHAR(32),
  version CHAR(3),
@@ -25,3 +27,5 @@ CREATE TABLE IF NOT EXISTS games(
  version CHAR(3),
  PRIMARY KEY (customerid, name, version),
  FOREIGN KEY (name, version) REFERENCES games(name, version) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED);
+ 
+*/
