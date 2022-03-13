83a1cef46909cc2bdaa543d6b2f77b98607b8df5
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-13 23:12:34 +0800

    Create type.sql

---
+++ sql/type.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO type VALUES('Office building');
+INSERT INTO type VALUES('Storage complex');
+INSERT INTO type VALUES('Warehouse');
+INSERT INTO type VALUES('Conference hall');
+INSERT INTO type VALUES('Studio room');
+INSERT INTO type VALUES('Apartment');
+INSERT INTO type VALUES('HDB');
