aba6627e2e572c7b30d1591f43ff026bc09109e5
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-13 23:25:21 +0800

    Update AppStoreClean.sql

--- sql/AppStoreClean.sql
+++ sql/AppStoreClean.sql
@@ -5,8 +5,8 @@
 *******************/
 
 DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS offices;
 DROP TABLE IF EXISTS units;
 DROP TABLE IF EXISTS features;
 DROP TABLE IF EXISTS type;
 DROP TABLE IF EXISTS address;
-DROP TABLE IF EXISTS offices;
