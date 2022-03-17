4483d1785be6540574c11ea5e7eac26867be6c5a
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-18 05:31:00 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -119,6 +119,7 @@ def edit(request, id):
             status = 'Customer edited successfully!'
             cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
             obj = cursor.fetchone()
+            return redirect('administrator')
 
     context["obj"] = obj
     context["status"] = status
