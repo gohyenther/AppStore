37681acbf4868daf1a8a576e6227aadb37d26113
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-20 17:12:19 +0800

    Update units.sql

--- sql/units.sql
+++ sql/units.sql
@@ -2,4 +2,4 @@
 INSERT INTO units VALUES('office space');
 INSERT INTO units VALUES('working cubicle');
 INSERT INTO units VALUES('conference room');
-INSERT INTO units VALUES('storage Space');
+INSERT INTO units VALUES('storage space');
