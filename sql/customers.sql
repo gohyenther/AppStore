e006c9b8175bcfef78ef8e036a6ff3fabe618d2f
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-20 17:10:28 +0800

    Update customers.sql

--- sql/customers.sql
+++ sql/customers.sql
@@ -5,3 +5,4 @@
 ****************************/
 
 INSERT INTO customers VALUES ('john1997', 'John', 'Ng', 'johnng1997@gmail.com', '1997-05-20', '98765432');
+INSERT INTO customers VALUES ('adamsmith', 'Adam', 'Smith', 'adamsmith@hotmail.com', '1995-11-22', '98291455');
