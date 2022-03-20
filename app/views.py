684cb9750c1cf561241b0c25567664aa9acf44d9
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-20 14:19:10 +0800

    Fixed login for customers

--- app/views.py
+++ app/views.py
@@ -54,7 +54,7 @@ def customerprofile(request, id):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
         customers = cursor.fetchall()
-        cursor.execute("SELECT * FROM offices ORDER BY unit WHERE occupier ISNULL")
+        cursor.execute("SELECT * FROM offices WHERE occupier ISNULL ORDER BY unit")
         offices = cursor.fetchall()
 
     result_dict = {'records': customers, 'offices': offices}
