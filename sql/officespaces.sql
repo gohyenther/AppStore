a6cccc128cf81b2269b121cc1157bced5690a80d
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-20 14:12:09 +0800

    Update officespaces.sql

--- sql/officespaces.sql
+++ sql/officespaces.sql
@@ -1,2 +1,2 @@
 
-INSERT INTO offices VALUES('office space', 'pantry', 'monthly', 'office building', 110, 'Prince Edward Park', '04-25', 120113, 'YES', 500);
+INSERT INTO offices VALUES('office space', 'pantry', 'monthly', 'office building', 110, 'Prince Edward Park', '04-25', 120113, NULL , 500);
