abc3be05d4c347c605883bb8daa78fd50b5a6053
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-20 15:00:49 +0800

    Fixed updating of rent table

--- app/views.py
+++ app/views.py
@@ -41,8 +41,8 @@ def customerprofile(request, id):
         if request.POST['action'] == 'rent':
             with connection.cursor() as cursor:
                 cursor.execute("UPDATE offices SET occupier = %s WHERE unit = %s AND street = %s AND unit_no = %s AND postal_code = %s",
-                               [id, request.POST['office_unit'], request.POST['office_street'], request.POST['office_unit_no'], request.POST['office_postal_code']])
-                cursor.execute("INSERT INTO rent VALUES (%s, %s, %s, %s, %s)",
+                               [id, request.POST['office_unit'], request.POST['office_street'], request.POST['office_unit_no'], request.POST['office_postal_code']],
+                              "INSERT INTO rent VALUES (%s, %s, %s, %s, %s)",
                                [id, request.POST['office_unit'], request.POST['office_street'], request.POST['office_unit_no'], request.POST['office_postal_code']])
          
     ## Vacate office space
