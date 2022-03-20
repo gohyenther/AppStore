d57632b7de67c3fbf8be24e0602c36b877bf507c
kahmeng1002 <87817100+kahmeng1002@users.noreply.github.com>
2022-03-21 03:00:52 +0800

    added 1 mock office

--- sql/officespaces.sql
+++ sql/officespaces.sql
@@ -1,2 +1,3 @@
 
 INSERT INTO offices VALUES('office space', 'pantry', 'monthly', 'office building', 110, 'Prince Edward Park', '04-25', 120113, NULL , 500);
+INSERT INTO offices VALUES('office space', 'projector', 'monthly', 'office building', 107, 'Prince George Park', '03-01', 118221, NULL, 300);
