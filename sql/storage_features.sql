8c9f688c23e376c097058ce7385de3a9b4d038a1
ball <65845980+angby509@users.noreply.github.com>
2022-03-21 23:42:03 +0800

    Update storage_features.sql

--- sql/storage_features.sql
+++ sql/storage_features.sql
@@ -1,1 +1,6 @@
-sfasfa
+INSERT INTO storage_features VALUES('shared space, CCTV Security, Security Access');
+INSERT INTO storage_features VALUES('shared space, CCTV Security');
+INSERT INTO storage_features VALUES('shared space');
+INSERT INTO storage_features VALUES('common space, CCTV Security, Security Access');
+INSERT INTO storage_features VALUES('common space, CCTV Security');
+INSERT INTO storage_features VALUES('common space');
