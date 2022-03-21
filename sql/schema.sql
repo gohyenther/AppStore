11cda5446901f1e6de9f77e375d8b63913f69e6c
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 03:48:22 +0800

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,7 +5,7 @@
 ********************/
 
 CREATE TABLE IF NOT EXISTS customers (
-	customerid VARCHAR(16) PRIMARY KEY,
+	customerid VARCHAR(64) PRIMARY KEY,
  	first_name VARCHAR(64) NOT NULL,
  	last_name VARCHAR(64) NOT NULL,
  	email VARCHAR(64) UNIQUE NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS storage_features(
  
 /* this address table exists only to be referrenced to ensure address is valid, removed rate*/
 CREATE TABLE IF NOT EXISTS address(
- street VARCHAR(32),
+ street VARCHAR(512),
  unit_no VARCHAR(32),
  postal_code NUMERIC(6),
  PRIMARY KEY(street, unit_no, postal_code));
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS offices(
  street VARCHAR(32),
  unit_no VARCHAR(32),
  postal_code NUMERIC(6),
- occupier VARCHAR(16),
+ occupier VARCHAR(64),
  rate NUMERIC(32),
  FOREIGN KEY (street, unit_no, postal_code) REFERENCES address(street, unit_no, postal_code) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  PRIMARY KEY (unit, street, unit_no, postal_code));
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS workcubes(
  street VARCHAR(32),
  unit_no VARCHAR(32),
  postal_code NUMERIC(6),
- occupier VARCHAR(16),
+ occupier VARCHAR(64),
  rate NUMERIC(32),
  FOREIGN KEY (street, unit_no, postal_code) REFERENCES address(street, unit_no, postal_code) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  PRIMARY KEY (unit, street, unit_no, postal_code));
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS confrooms(
  street VARCHAR(32),
  unit_no VARCHAR(32),
  postal_code NUMERIC(6),
- occupier VARCHAR(16),
+ occupier VARCHAR(64),
  rate NUMERIC(32),
  FOREIGN KEY (street, unit_no, postal_code) REFERENCES address(street, unit_no, postal_code) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  PRIMARY KEY (unit, street, unit_no, postal_code));
@@ -109,7 +109,7 @@ CREATE TABLE IF NOT EXISTS storages(
  street VARCHAR(32),
  unit_no VARCHAR(32),
  postal_code NUMERIC(6),
- occupier VARCHAR(16),
+ occupier VARCHAR(64),
  rate NUMERIC(32),
  FOREIGN KEY (street, unit_no, postal_code) REFERENCES address(street, unit_no, postal_code) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  PRIMARY KEY (unit, street, unit_no, postal_code));
@@ -117,7 +117,7 @@ CREATE TABLE IF NOT EXISTS storages(
  
 /* if it exists in the table rent, then occupier = NULL in office spaces, workcubes, confrooms, storages, unf_units */
 CREATE TABLE IF NOT EXISTS rent(
- customerid VARCHAR(16) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+ customerid VARCHAR(64) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  unit VARCHAR(32) REFERENCES units(purpose) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  street VARCHAR(32),
  unit_no VARCHAR(32),
@@ -129,13 +129,13 @@ CREATE TABLE IF NOT EXISTS rent(
 	
 /* transaction == payment table */
 CREATE TABLE IF NOT EXISTS transaction(
- customerid VARCHAR(16) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+ customerid VARCHAR(64) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  transactionid VARCHAR(18) PRIMARY KEY,
  amount_paid NUMERIC(64) NOT NULL);
  
  
 /* login credentials */
 CREATE TABLE IF NOT EXISTS login(
- username VARCHAR(16) REFERENCES customers(customerid),
- password VARCHAR(16) NOT NULL);
+ username VARCHAR(64) REFERENCES customers(customerid),
+ password VARCHAR(64) NOT NULL);
  
