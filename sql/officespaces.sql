343cb1fc9a62626897ee69c6a44aaefbece1e3fc
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 04:36:55 +0800

    Update officespaces.sql

--- sql/officespaces.sql
+++ sql/officespaces.sql
@@ -1,3 +1,4 @@
 
-INSERT INTO offices VALUES('office space','fully-furnished, air-con, high speed Wi-Fi, pantry, restroom, near MRT' , 'monthly', 'office building', '110', 'Prince Edward Park', '04-25', '120113', NULL , '500');
-INSERT INTO offices VALUES('office space', 'fully-furnished, air-con, high speed Wi-Fi, pantry, meeting room, projector, near MRT', 'monthly', 'office building', '107', 'Prince George Park', '03-01', '118221', NULL, '300');
+INSERT INTO offices VALUES('office space','fully-furnished, air-con, high speed Wi-Fi, pantry, restroom, near MRT' , 'monthly', 'office building', '110', 'Prince Edward Park', '04-25', '120113', 'john1997' , '500');
+INSERT INTO offices VALUES('office space', 'fully-furnished, air-con, high speed Wi-Fi, pantry, meeting room, projector, near MRT', 'monthly', 'office building', '107', 'Prince George Park', '03-01', '118221', NULL, '400');
+INSERT INTO offices VALUES('office space', 'unfurnished, air-con, near MRT', 'monthly' 'office building', '150', '5 Yishun Industrial Park', '01-188', '763250', NULL, '300');
