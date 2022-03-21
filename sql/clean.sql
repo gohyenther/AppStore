f147955372a626b480597002c75b381d6d309ab5
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 02:25:00 +0800

    Update clean.sql

--- sql/clean.sql
+++ sql/clean.sql
@@ -5,9 +5,13 @@
 *******************/
 
 DROP TABLE IF EXISTS offices;
+DROP TABLE IF EXISTS office_features;
 DROP TABLE IF EXISTS workcubes;
+DROP TABLE IF EXISTS workcube_features;
 DROP TABLE IF EXISTS confrooms;
+DROP TABLE IF EXISTS confroom_features;
 DROP TABLE IF EXISTS storages;
+DROP TABLE IF EXISTS storage_features;
 DROP TABLE IF EXISTS unf_units;
 DROP TABLE IF EXISTS rent;
 DROP TABLE IF EXISTS units;
@@ -17,5 +21,3 @@ DROP TABLE IF EXISTS address;
 DROP TABLE IF EXISTS transaction;
 DROP TABLE IF EXISTS login;
 DROP TABLE IF EXISTS customers;
-
-
