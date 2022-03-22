4154cf2f0b3077f28b6ef7d9e1b3ed1253f2fa96
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 15:03:01 +0800

    Update officespaces.sql

--- sql/officespaces.sql
+++ sql/officespaces.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO offices VALUES('office space','fully-furnished, air-con, high speed Wi-Fi, pantry, restroom, near MRT' , 'monthly', 'office building', '110', 'Prince Edward Park', '04-25', '120113', 'john1997' , '500');
-INSERT INTO offices VALUES('office space', 'fully-furnished, air-con, high speed Wi-Fi, pantry, meeting room, projector, near MRT', 'monthly', 'office building', '107', 'Prince George Park', '03-01', '118221', NULL, '400');
-INSERT INTO offices VALUES('office space', 'unfurnished, air-con, near MRT', 'monthly', 'office building', '150', '5 Kings Point', '65-02', '488609', NULL, '300');
+INSERT INTO offices VALUES('office space','fully-furnished, air-con, high speed Wi-Fi, pantry, restroom, near MRT' , 'monthly', 'office building', '110', 'Prince Edward Park', '04-25', '120113', 'Yes' , '500');
+INSERT INTO offices VALUES('office space', 'fully-furnished, air-con, high speed Wi-Fi, pantry, meeting room, projector, near MRT', 'monthly', 'office building', '107', 'Prince George Park', '03-01', '118221', 'No', '400');
+INSERT INTO offices VALUES('office space', 'unfurnished, air-con, near MRT', 'monthly', 'office building', '150', '5 Kings Point', '65-02', '488609', 'No', '300');
