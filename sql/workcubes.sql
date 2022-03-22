31c298f4509386fe6334b17c46f688119c78b16f
ball <65845980+angby509@users.noreply.github.com>
2022-03-22 16:13:59 +0800

    Update workcubes.sql

--- sql/workcubes.sql
+++ sql/workcubes.sql
@@ -1,3 +1,4 @@
 INSERT INTO workcubes(unit, features, timescale type, size_sf, street, unit_no, postal_code, occupier, rate)
-SELECT FROM temp(unit, features, timescale, type, size_sf, street, unit_no, postal_code, occupier, rate)
-WHERE unit = 'working cubicle';
+SELECT unit, features, timescale, type, size_sf, street, unit_no, postal_code, occupier, rate
+FROM temp
+WHERE unit = 'Working cubicle';
