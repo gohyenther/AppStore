324a39b6d25cc80efeeb62b28ea8fa5e3ecc842e
kahmeng1002 <87817100+kahmeng1002@users.noreply.github.com>
2022-03-23 01:01:24 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -58,7 +58,7 @@ def customerprofile(request, id):
     if request.POST:
         if request.POST['action'] == 'pricehighlow':
             with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM offices, workcubes, confrooms, storages AS buildings ORDER BY buildings.rate DESC")
+                cursor.execute("(SELECT * FROM offices UNION SELECT * FROM workcubes UNION SELECT * FROM confrooms UNION SELECT * FROM storages) ORDER BY rate DESC")
                 offices_price_highlow = cursor.fetchall()
         if request.POST['action'] == 'pricelowhigh':
             with connection.cursor() as cursor:
