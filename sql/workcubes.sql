4018995ef60025423a708b0678f0e65132e48962
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 16:08:41 +0800

    fixed typo

--- sql/workcubes.sql
+++ sql/workcubes.sql
@@ -1,3 +1,3 @@
 INSERT INTO workcubes(unit, features, timescale type, size_sf, street, unit_no, postal_code, occupier, rate)
 SELECT FROM temp(unit, features, timescale, type, size_sf, street, unit_no, postal_code, occupier, rate)
-WHERE unit = 'Working cubicle';
+WHERE unit = 'working cubicle';
