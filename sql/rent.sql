6f2c2167f81353e5f14acafd6050e17b5bfa5c5f
ball <65845980+angby509@users.noreply.github.com>
2022-03-22 18:03:27 +0800

    Update rent.sql

--- sql/rent.sql
+++ sql/rent.sql
@@ -7,7 +7,7 @@ SELECT cust.customerid, t.unit, t.street, t.unit_no, t.postal_code
 FROM (SELECT customerid, FLOOR(RANDOM()*(999))+1
         FROM customers
         ORDER BY RANDOM()
-        LIMIT 1) cust, temp t
+        LIMIT 1) AS cust, temp t
 WHERE occupier = 'Yes';
 
 
