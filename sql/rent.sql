8f1859c6a808fdf73df140d3e27f3589803c0e2b
ball <65845980+angby509@users.noreply.github.com>
2022-03-22 16:09:32 +0800

    Update rent.sql

--- sql/rent.sql
+++ sql/rent.sql
@@ -1,2 +1,9 @@
 
-insert into rent (customerid, unit, street, unit_no, postal_code) values ('john1997', 'office space', 'Prince Edward Park', '04-25', '120113');
+/*insert into rent (customerid, unit, street, unit_no, postal_code) values ('john1997', 'office space', 'Prince Edward Park', '04-25', '120113');*/
+
+
+INSERT INTO rent(customerid, unit, street, unit_no, postal_code)
+SELECT FROM customers(customer_id),temp(unit, street, unit_no, postal_code)
+FROM customers c, temp t
+WHERE r.customer_id = c.customerid AND
+occupier = 'Yes';
