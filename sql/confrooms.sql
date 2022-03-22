fdb6c7c323cd519e97fd3a73602b5c9f63e90acb
ball <65845980+angby509@users.noreply.github.com>
2022-03-22 16:01:21 +0800

    Update confrooms.sql

--- sql/confrooms.sql
+++ sql/confrooms.sql
@@ -1,1 +1,3 @@
-
+INSERT INTO confrooms(unit, features, timescale type, size_sf, street, unit_no, postal_code, occupier, rate)
+SELECT FROM temp(unit, features, timescale type, size_sf, street, unit_no, postal_code, occupier, rate)
+WHERE unit = 'Conference room';
