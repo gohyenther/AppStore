d602a0c7e06e6816ec1be1b42756d0d72b43852e
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 16:44:33 +0800

    Update rent.sql

--- sql/rent.sql
+++ sql/rent.sql
@@ -3,14 +3,10 @@
 
 
 INSERT INTO rent(customerid, unit, street, unit_no, postal_code)
-SELECT '0', unit, street, unit_no, postal_code
+SELECT (SELECT customerid, FLOOR(RANDOM()*(999))+1
+        FROM customers
+        ORDER BY RANDOM()
+        LIMIT 1) , unit, street, unit_no, postal_code
 FROM temp 
 WHERE occupier = 'Yes';
-
-UPDATE rent
-SET customerid = 
-(SELECT customerid, FLOOR(RANDOM()*(999))+1
-FROM customers
-ORDER BY RANDOM()
-LIMIT 1);
 
