bdf16e83a603da70c160a863bf1a3f509e766abc
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-22 16:22:58 +0800

    Update type.sql

--- sql/type.sql
+++ sql/type.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO type VALUES('office building');
-INSERT INTO type VALUES('storage complex');
-INSERT INTO type VALUES('warehouse');
-INSERT INTO type VALUES('conference hall');
-INSERT INTO type VALUES('studio room');
-INSERT INTO type VALUES('apartment');
+INSERT INTO type VALUES('Office building');
+INSERT INTO type VALUES('Storage complex');
+INSERT INTO type VALUES('Warehouse');
+INSERT INTO type VALUES('Conference hall');
+INSERT INTO type VALUES('Studio room');
+INSERT INTO type VALUES('Apartment');
 INSERT INTO type VALUES('HDB');
