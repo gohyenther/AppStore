28347f285c1ca9ec4ba2990d959e9bfc0dab8e40
Goh Yen Ther <65606330+gohyenther@users.noreply.github.com>
2022-03-27 18:47:56 +0800

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -22,6 +22,8 @@ CREATE TABLE IF NOT EXISTS type(
  classification VARCHAR(32) PRIMARY KEY);
  
  
+/* not needed anymore
+ 
 CREATE TABLE IF NOT EXISTS office_features(
  features VARCHAR(512) PRIMARY KEY);
 
@@ -36,6 +38,8 @@ CREATE TABLE IF NOT EXISTS confroom_features(
  
 CREATE TABLE IF NOT EXISTS storage_features(
  features VARCHAR(512) PRIMARY KEY);
+ 
+*/
  
  
 /* this address table exists only to be referrenced to ensure address is valid, removed rate*/
