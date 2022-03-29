bdddae884d9d09f42f1d157fb9ae1caca0188c09
ball <65845980+angby509@users.noreply.github.com>
2022-03-29 20:27:00 +0800

    Update transaction.sql

--- sql/transaction.sql
+++ sql/transaction.sql
@@ -1,12 +1,11 @@
 INSERT INTO transaction
-SELECT r.customerid, r.customerid, 
-(CASE WHEN t.timescale = 'Monthly'
- THEN ((DATE_PART('year', t.end_date::date) - DATE_PART('year', t.start_date::date)) * 12 +
+SELECT r.customerid, r.customerid,
+			(CASE WHEN t.timescale = 'Monthly'
+			 THEN ((DATE_PART('year', t.end_date::date) - DATE_PART('year', t.start_date::date)) * 12 +
               (DATE_PART('month', t.end_date::date) - DATE_PART('month', t.start_date::date))) * t.rate
- CASE WHEN t.timescale = 'Weekly'
- THEN (TRUNC(DATE_PART('day', t.end_date::timestamp - t.start_date::timestamp)/7)) * t.rate
- ELSE (DATE_PART('day', t.end_date::timestamp - t.start_date::timestamp) * 24 + 
-              DATE_PART('hour', t.end_date::timestamp - t.start_date::timestamp)) * t.rate
- ) AS total
+			 WHEN t.timescale = 'Weekly'
+			 THEN (TRUNC(DATE_PART('day', t.end_date::timestamp - t.start_date::timestamp)/7)) * t.rate
+			 ELSE (DATE_PART('day', t.end_date::timestamp - t.start_date::timestamp) * 24 + 
+              DATE_PART('hour', t.end_date::timestamp - t.start_date::timestamp)) * t.rate) AS total
 FROM rent r, temp t
 WHERE r.customerid = t.customerid;
