73e78646754611da1c1783d22a12ff729faf6619
ball <65845980+angby509@users.noreply.github.com>
2022-03-30 15:19:49 +0800

    updated primary key for transaction table

--- sql/temp.sql
+++ sql/temp.sql
@@ -998,8 +998,9 @@ WHERE street = '64 Debs Park' AND unit_no = '91-76' AND postal_code = '372326';
 /* transaction == payment table */
 CREATE TABLE IF NOT EXISTS transaction(
  customerid VARCHAR(64) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
- transactionid VARCHAR(18) PRIMARY KEY,
- amount_paid NUMERIC(64) NOT NULL);
+ transactionid VARCHAR(18),
+ amount_paid NUMERIC(64) NOT NULL,
+ PRIMARY KEY (customerid, transactionid, amount_paid));
 
 
 
