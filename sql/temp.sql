78938831720d6be7696fc9dd335bdca2137e6c74
ball <65845980+angby509@users.noreply.github.com>
2022-03-30 15:10:19 +0800

    Update temp.sql

--- sql/temp.sql
+++ sql/temp.sql
@@ -921,31 +921,76 @@ CREATE TABLE IF NOT EXISTS rent(
  /* REFERENCES TO EITHER offices, workcubes, confrooms, storages or unf_units */
  FOREIGN KEY (street, unit_no, postal_code) REFERENCES address(street, unit_no, postal_code) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED);
 
-/* this table for generating data in rent table purpose only new user only insert into rent table */
-CREATE TABLE IF NOT EXISTS datetime(
-	start_time VARCHAR(64),
-	end_time VARCHAR(64),
-	PRIMARY KEY(start_time, end_time));
 
-/* 10 insertions */
-insert into datetime (start_time, end_time) values ('2021-11-14 00:31:58', '2021-05-11 00:02:15');
-insert into datetime (start_time, end_time) values ('2021-10-30 17:38:55', '2022-03-04 19:01:07');
-insert into datetime (start_time, end_time) values ('2021-12-10 05:00:16', '2021-07-21 04:31:21');
-insert into datetime (start_time, end_time) values ('2021-12-27 05:51:57', '2021-06-11 12:01:17');
-insert into datetime (start_time, end_time) values ('2021-10-10 00:39:44', '2021-08-11 10:02:17');
-insert into datetime (start_time, end_time) values ('2021-10-25 17:36:37', '2021-10-06 02:36:30');
-insert into datetime (start_time, end_time) values ('2021-11-06 04:14:22', '2021-06-12 02:01:32');
-insert into datetime (start_time, end_time) values ('2021-10-21 19:21:15', '2022-01-24 00:04:08');
-insert into datetime (start_time, end_time) values ('2021-11-11 02:40:07', '2021-03-26 06:09:43');
-insert into datetime (start_time, end_time) values ('2021-10-15 08:45:12', '2021-07-04 02:55:06');
-
-
-
-
+/* random customer inserted to avoid null constraint */
 INSERT INTO rent
-SELECT c2.customerid, t.unit, dt.start_time, dt.end_time, t.street, t.unit_no, t.postal_code
-FROM (SELECT c1.customerid FROM customers c1 ORDER BY RANDOM() LIMIT 10) AS c2, datetime dt, temp t
+SELECT 'random customer', t.unit, 'random start time', 'random end time', t.street, t.unit_no, t.postal_code
+FROM temp t
 WHERE t.occupier = 'Yes';
+
+/* 10 rented units with customers */
+UPDATE rent
+SET customerid = 'bburehillkx',
+start_rent = '2021-11-14 00:31:58', 
+end_rent = '2021-05-11 00:02:15'
+WHERE street = '92 Briar Crest Way' AND unit_no = '47-10' AND postal_code = '837954';
+
+/* to demonstrate one customer can rent multiple units */
+UPDATE rent
+SET customerid = 'bburehillkx',
+start_rent = '2021-10-30 17:38:55', 
+end_rent = '2022-03-04 19:01:07'
+WHERE street = '65 Burrows Plaza' AND unit_no = '91-94' AND postal_code = '428856';
+
+/* remaining 8 rents */
+
+UPDATE rent
+SET customerid = 'amaldenqa',
+start_rent = '2021-12-10 05:00:16', 
+end_rent = '2021-07-21 04:31:21'
+WHERE street = '2 Sheridan Court' AND unit_no =  '13-00' AND postal_code = '504652';
+
+UPDATE rent
+SET customerid = 'ubernardouxob',
+start_rent = '2021-12-27 05:51:57', 
+end_rent = '2021-06-11 12:01:17'
+WHERE street = '041 Northfield Park' AND unit_no = '52-26' AND postal_code = '328128';
+
+UPDATE rent
+SET customerid = 'asamsinil',
+start_rent = '2021-10-10 00:39:44', 
+end_rent = '2021-08-11 10:02:17'
+WHERE street = '7133 Continental Place' AND unit_no = '71-12' AND postal_code = '336937';
+
+UPDATE rent
+SET customerid = 'kdederichol',
+start_rent = '2021-10-25 17:36:37', 
+end_rent = '2021-10-06 02:36:30'
+WHERE street = '16398 Valley Edge Pass' AND unit_no = '63-16' AND postal_code = '798609';
+
+UPDATE rent
+SET customerid = 'cmapowderaz',
+start_rent = '2021-11-06 04:14:22', 
+end_rent = '2021-06-12 02:01:32'
+WHERE street = '95 Porter Parkway' AND unit_no = '80-52' AND postal_code = '325442';
+
+UPDATE rent
+SET customerid = 'jhovecl',
+start_rent = '2021-10-21 19:21:15', 
+end_rent = '2022-01-24 00:04:08'
+WHERE street = '38 Northridge Junction' AND unit_no = '50-26' AND postal_code = '267445';
+
+UPDATE rent
+SET customerid = 'listed9b',
+start_rent = '2021-11-11 02:40:07', 
+end_rent = '2021-03-26 06:09:43'
+WHERE street = '0717 Hovde Road' AND unit_no = '03-43' AND postal_code = '673133';
+
+UPDATE rent
+SET customerid = 'hjezardkb',
+start_rent = '2021-10-15 08:45:12', 
+end_rent = '2021-07-04 02:55:06'
+WHERE street = '64 Debs Park' AND unit_no = '91-76' AND postal_code = '372326';
 
 
  
