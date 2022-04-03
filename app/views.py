187dc5d7ba37b8f514e24d4b0d602e4f1585d187
kahmeng1002 <87817100+kahmeng1002@users.noreply.github.com>
2022-04-03 21:27:34 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -571,7 +571,7 @@ def adminanalytics(request):
                 customer_rented = cursor.fetchall()
         if request.POST['action'] == 'no_rent':
             with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM customers c EXCEPT SELECT * FROM rent r")
+                cursor.execute("SELECT c.customerid FROM customers c EXCEPT SELECT r.customerid FROM rent r")
                 customer_rented = cursor.fetchall()
                 
     if request.POST:
